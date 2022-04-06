53f5af57865bf63eaf48a86e1a182fc57475830f
Serhii <zarka08@gmail.com>
2022-04-06 18:45:15 +0400

    project3 worked

--- src/components/Project3/Product.vue
+++ src/components/Project3/Product.vue
@@ -2,19 +2,21 @@
   <div>
     <Header/>
     <my-header></my-header>
-    <h5> Код товара: {{ $route.params.id}}</h5>
     <div class="row">
       <div class="col-md-5 col-md-offset-0">
-        <figure>
+        <figure style="text-align: center">
           <img class="product" :src="product.image" >
         </figure>
+        <h5> Код товара: {{ $route.params.id}}</h5>
       </div>
       <div class="col-md-6 col-md-offset-0 description">
         <h1>{{product.title}}</h1>
         <p v-html="product.description"></p>
+        
         <p class="price">Стоимость:  {{product.price }}  грн</p>
+        
         <!-- <button @click="edit">Редактировать</button> -->
-        <router-view></router-view> 
+        <!-- <router-view></router-view>  -->
       </div>
     </div>
   </div>
@@ -43,13 +45,8 @@ import Header from '@/components/Header.vue';
   }
 </script>
 <style scoped>
-  h1{
-    height: 10rem;
-  }
-
   h5{
-    color:blueviolet;
+    color:gold;
     text-align: center;
   }
-
 </style>
