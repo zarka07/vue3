2e798fe94f57e1544c75256dfe96e3736cc56d95
Serhii <zarka08@gmail.com>
2023-02-08 20:21:07 +0200

    feat!: Turn on Composition API into Main.vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,32 +6,11 @@
   </div>
 </template>
 
-<script>
-import { UserStore } from "@/stores/UserStore";
+<script setup>
 import Header from "@/components/Header.vue";
 import CV from "@/components/Project1/CV.vue";
 import Footer from "@/components/Footer.vue";
 
-export default {
-  name: "MainItem",
-  components: {
-    Header,
-    CV,
-    Footer,
-  },
-  setup() {
-    const mainStore = UserStore();
-
-    return {
-      mainStore,
-    };
-  },
-  data() {
-    return {
-      email: this.mainStore.email,
-    };
-  },
-};
 </script>
 <style scoped>
 .mxauto {
